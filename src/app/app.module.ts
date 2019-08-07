70c49591f1cc3e3a234d7de4350b12723c4d2367
Ana Ortega <aortega@asgbpm.com>
2019-08-07 13:27:08 -0500

    Fin de efectos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+// NgRx
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { appReducers } from './store/app.reducer';
+import { effectsArr } from './store/effects/index';
+
+// Rutas
 import { AppRoutingModule } from './app-routing.module';
 
 // Modulos Personalizados
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
-import { HttpClientModule } from '@angular/common/http';
+
+// Environment
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,6 +29,12 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    StoreModule.forRoot( appReducers ),
+    EffectsModule.forRoot( effectsArr ),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
     SharedModule,
     UsuariosModule,
 
--- src/app/models/usuario.model.ts
+++ src/app/models/usuario.model.ts
@@ -1,9 +1,5 @@
 
 export class Usuario {
-    // public id: number;
-    // public first_name: string;
-    // public last_name: string;
-    // public avatar: string;
 
     constructor(
         public id: number,
